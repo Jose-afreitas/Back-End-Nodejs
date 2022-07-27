85a362642c1c3392e3a838c093c16d9901b5050e
Jose-afreitas <josealmeida106@gmail.com>
2022-07-27 13:52:27 -0300

    wip- Installing express in the project

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ let server = http.createServer((req, res) => {
     console.log('URL', req.url);
     console.log('METHOD', req.method);
 
-    res.end('OK');
+    switch (req.url) {
 
+        case '/':
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1> Hello </>');
+            break;
+
+        case '/users':
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+
+                users: [{
+                    name: 'José Augusto',
+                    email: 'joseaugusto@teste.com.br',
+                    id: 1
+                }]
+            }));
+
+            break;
+
+    }
 });
 
 server.listen(3000, '127.0.0.1', () => {
