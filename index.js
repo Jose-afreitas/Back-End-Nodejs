a8ee1336d47347256934cc1273acba96765aec3d
Jose-afreitas <josealmeida106@gmail.com>
2022-07-27 14:04:25 -0300

    wip - installing nodemon and connecting server with express

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-const http = require('http');
-let server = http.createServer((req, res) => {
+const express = require('express');
+let app = express();
 
-    console.log('URL', req.url);
-    console.log('METHOD', req.method);
 
-    switch (req.url) {
+app.get('/', (req, res) => {
 
-        case '/':
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1> Hello </>');
 
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1> Hello </>');
-            break;
-
-        case '/users':
-
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-
-                users: [{
-                    name: 'José Augusto',
-                    email: 'joseaugusto@teste.com.br',
-                    id: 1
-                }]
-            }));
-
-            break;
-
-    }
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.get('/users', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+
+        users: [{
+            name: 'José Augusto',
+            email: 'joseaugusto@teste.com.br',
+            id: 1
+        }]
+    });
+});
+
+app.listen(3000, '127.0.0.1', () => {
 
     console.log('Servidor Rodando !')
 })
